8dd9afa399dee70a4ed1cb1ab8246ffc089c5288
Olivier Gintrand <olivier.gintrand@forterro.com>
2026-04-03 15:20:11 +0200

    alembic: merge Forterro RC3 migration heads
    Merge migration combining 3 Forterro topic branch heads:
    - personal-credential-store (a1b2c3d4e5f6)
    - gateway-tool-filters (b3c4d5e6f7a8)
    - team-management OIDC sync (f7a2b1c3d4e5)

---
+++ mcpgateway/alembic/versions/merge_forterro_rc3_heads.py
@@ -0,0 +1,26 @@
+"""Merge Forterro RC3 migration heads
+
+Revision ID: forterro_rc3_merge
+Revises: a1b2c3d4e5f6, b3c4d5e6f7a8, f7a2b1c3d4e5
+Create Date: 2026-04-03 14:00:00.000000
+
+"""
+from typing import Sequence, Union
+
+# revision identifiers, used by Alembic.
+revision: str = "forterro_rc3_merge"
+down_revision: Union[str, Sequence[str], None] = (
+    "a1b2c3d4e5f6",
+    "b3c4d5e6f7a8",
+    "f7a2b1c3d4e5",
+)
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
