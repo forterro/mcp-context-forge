92c65a91dd6e990d8a18ae845b33e134f717605d
Olivier Gintrand <olivier.gintrand@forterro.com>
2026-04-28 10:56:10 +0200

    fix: include upstream heads a5b1cec45457 and bb43712cae28 in Alembic merge migration

--- mcpgateway/alembic/versions/merge_7_heads_rc4_integration.py
+++ mcpgateway/alembic/versions/merge_7_heads_rc4_integration.py
@@ -1,7 +1,7 @@
-"""Merge seven divergent Alembic heads for RC4 integration.
+"""Merge all divergent Alembic heads for RC4 integration.
 
 Revision ID: 664ce26d3339
-Revises: 5126ced48fd0, 8b22c624dd73, a8f3b2c1d4e5, aa1b2c3d4e5f, b3c4d5e6f7a8, k5e6f7g8h9i0, q1b2c3d4e5f6
+Revises: 5126ced48fd0, 8b22c624dd73, a8f3b2c1d4e5, aa1b2c3d4e5f, b3c4d5e6f7a8, k5e6f7g8h9i0, q1b2c3d4e5f6, a5b1cec45457, bb43712cae28
 Create Date: 2026-04-28
 
 """
@@ -16,6 +16,8 @@ down_revision = (
     "b3c4d5e6f7a8",
     "k5e6f7g8h9i0",
     "q1b2c3d4e5f6",
+    "a5b1cec45457",
+    "bb43712cae28",
 )
 branch_labels = None
 depends_on = None
