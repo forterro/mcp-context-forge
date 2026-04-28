d202cae13f08161c53968fa1c7a73405330ff13b
Olivier Gintrand <olivier.gintrand@forterro.com>
2026-04-28 10:45:43 +0200

    fix: add Alembic merge migration to resolve seven divergent heads

---
+++ mcpgateway/alembic/versions/merge_7_heads_rc4_integration.py
@@ -0,0 +1,29 @@
+"""Merge seven divergent Alembic heads for RC4 integration.
+
+Revision ID: 664ce26d3339
+Revises: 5126ced48fd0, 8b22c624dd73, a8f3b2c1d4e5, aa1b2c3d4e5f, b3c4d5e6f7a8, k5e6f7g8h9i0, q1b2c3d4e5f6
+Create Date: 2026-04-28
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = "664ce26d3339"
+down_revision = (
+    "5126ced48fd0",
+    "8b22c624dd73",
+    "a8f3b2c1d4e5",
+    "aa1b2c3d4e5f",
+    "b3c4d5e6f7a8",
+    "k5e6f7g8h9i0",
+    "q1b2c3d4e5f6",
+)
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
