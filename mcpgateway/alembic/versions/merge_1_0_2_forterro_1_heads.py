0ec893c4c761a6f803f4e6e008b3a9cd65d8596f
Olivier Gintrand <olivier.gintrand@forterro.com>
2026-05-27 17:59:06 +0200

    fix(alembic): include a5b1cec45457 and e28566875fa4 in merge migration
    The merge migration was missing two heads:
    - a5b1cec45457 (rename oidc_group_id to oidc_group_ids from team-management)
    - e28566875fa4 (add deprecated flag to tools from upstream)
    
    This caused 'Multiple head revisions' error at startup.
    
    Signed-off-by: Olivier Gintrand <olivier.gintrand@forterro.com>

--- mcpgateway/alembic/versions/merge_1_0_2_forterro_1_heads.py
+++ mcpgateway/alembic/versions/merge_1_0_2_forterro_1_heads.py
@@ -1,7 +1,7 @@
-"""Merge 7 heads into single head for 1.0.2-forterro-1 integration
+"""Merge all heads into single head for 1.0.2-forterro integration
 
 Revision ID: f1020001merge
-Revises: 5126ced48fd0, 8b22c624dd73, a8f3b2c1d4e5, aa1b2c3d4e5f, b3c4d5e6f7a8, k5e6f7g8h9i0, q1b2c3d4e5f6
+Revises: 5126ced48fd0, 8b22c624dd73, a8f3b2c1d4e5, aa1b2c3d4e5f, b3c4d5e6f7a8, k5e6f7g8h9i0, q1b2c3d4e5f6, a5b1cec45457, e28566875fa4
 Create Date: 2026-05-27
 """
 
@@ -17,6 +17,8 @@ down_revision = (
     "b3c4d5e6f7a8",
     "k5e6f7g8h9i0",
     "q1b2c3d4e5f6",
+    "a5b1cec45457",
+    "e28566875fa4",
 )
 branch_labels = None
 depends_on = None
